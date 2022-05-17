6711fa57143c4e1e5f6dec139295a4fd8fc3fb27
Guochiuan <guochiuan.wang@gmail.com>
2022-05-17 20:07:04 +0800

    third

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import Card from './Card';
+import { robots } from './robots';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,13 +15,15 @@ root.render(
 
   <React.Fragment>
     <h1>Hello worfghghgfhghfghfld</h1>,
-
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
 
     <Hello greeting={'sdfdskfsd;kfdsf' + 'dfsfdsfsdffsd'} />
 
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+
+    <App />
+
   </React.Fragment>
 
 );
