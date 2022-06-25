022cb7d2cea4d6c93a75ec97f28f25db9c529122
Guochiuan <guochiuan.wang@gmail.com>
2022-06-25 11:25:29 +0800

    Updates

---
+++ precache-manifest.e09b00f88ee704fbaffcc9798f86c90c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4480bfec05c24c60a2a6",
+    "url": "/ReactPractice/static/css/main.620fcfd7.chunk.css"
+  },
+  {
+    "revision": "4480bfec05c24c60a2a6",
+    "url": "/ReactPractice/static/js/main.8e48efc9.chunk.js"
+  },
+  {
+    "revision": "f194e41d62172f179616",
+    "url": "/ReactPractice/static/js/runtime~main.45973db8.js"
+  },
+  {
+    "revision": "303bf6cb2c340ef58aa3",
+    "url": "/ReactPractice/static/css/2.f5fb795d.chunk.css"
+  },
+  {
+    "revision": "303bf6cb2c340ef58aa3",
+    "url": "/ReactPractice/static/js/2.25284d51.chunk.js"
+  },
+  {
+    "revision": "7b4b590765ba9cc9d7de",
+    "url": "/ReactPractice/static/js/3.f1bbbdc7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ReactPractice/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ac1f2ff553d953ee0b8caa175f88519d",
+    "url": "/ReactPractice/index.html"
+  }
+];
